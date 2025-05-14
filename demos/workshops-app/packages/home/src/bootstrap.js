492e272bc32226e21524f40792143dfd6f046092
Prashanth Puranik <puranik@digdeeper.in>
2025-05-14 13:20:11 +0530

    store bug fix

--- demos/workshops-app/packages/home/src/bootstrap.js
+++ demos/workshops-app/packages/home/src/bootstrap.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { store } from 'shared/store';
+import store from 'shared/store';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
